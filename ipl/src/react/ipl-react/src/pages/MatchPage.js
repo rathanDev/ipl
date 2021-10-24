397b7d86d5d9b653f5190bebe9e6e4f209f5674c
jana <rathan.kparam@gmail.com>
2021-10-24 19:09:57 +0800

    Add MatchPage use fetch async await

--- ipl/src/react/ipl-react/src/pages/MatchPage.js
+++ ipl/src/react/ipl-react/src/pages/MatchPage.js
@@ -1,7 +1,30 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
+import { useParams } from "react-router";
+import MatchDetailCard from "../components/MatchDetailCard";
 
 const MatchPage = () => {
-  return (<h1>MatchPage</h1>);
+  const [matches, setMatches] = useState([]);
+  const { teamName, year } = useParams();
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(
+        `http://localhost:8080/team/${teamName}/matches?year=${year}`
+      );
+      const data = await response.json();
+      setMatches(data);
+    };
+    fetchMatches();
+  }, []);
+
+  return (
+    <div className="MatchPage">
+      <h1>Match Page</h1>
+      {matches.map((m) => (
+        <MatchDetailCard teamName={teamName} match={m} />
+      ))}
+    </div>
+  );
 };
 
 export default MatchPage;
